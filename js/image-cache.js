bdf11fb07c571ebb0e98ee68c8d9b327b1e571eb
Dat Chung <datxchung@gmail.com>
2015-12-26 16:01:22 -0500

    Implement 2 stage caching with localStorage (for persisting recent images) and sessionStorage for less recent images

--- js/image-cache.js
+++ js/image-cache.js
@@ -1,5 +1,25 @@
+/**
+ * Wrapper for two stage caching.
+ *
+ * Stage 1 (localStorage):
+ * Images will be cached and persisted even after ending the session for up to CACHE_PERSIST_DAYS
+ * from the current date.
+ *
+ * Stage 2 (sessionStorage):
+ * Images will be cached (but not persisted) regardless of age.
+ *
+ * Example use case:
+ * # User browses images for previous days up to CACHE_PERSIST_DAYS ago.
+ * ** These images are cached and persisted (stage 1).
+ * # User browses images further than CACHE_PERSIST_DAYS ago.
+ * ** These images are cached but not persisted (stage 2).
+ * # User browses back to an image.
+ * ** Image loads from stage 2 cache.
+ * # User browses back to an image within CACHE_PERSIST_DAYS ago.
+ * ** Image loads from stage 1 cache.
+ */
 var imageCache = (function (my) {
-    var CACHE_PERSIST_DAYS = 10;
+    var CACHE_PERSIST_DAYS = 7;
 
     var getKey = function(date) {
         return date.toISOString().substring(0, 10);
@@ -9,6 +29,15 @@ var imageCache = (function (my) {
         first.setHours(0, 0, 0, 0);
         second.setHours(0, 0, 0, 0);
         return Math.round((second - first) / (1000 * 60 * 60 * 24));
+    };
+
+    var trySet = function(storage, date, image) {
+        try {
+            storage.setItem(getKey(date), image);
+        }
+        catch(e) {
+            console.log('Cache is full. ', e);
+        }
     };
 
     /**
@@ -31,20 +60,22 @@ var imageCache = (function (my) {
     my.set = function(date, image) {
         var today = new Date();
 
-        if(daysBetween(date, today) <= CACHE_PERSIST_DAYS) {
-            try {
-                localStorage.setItem(getKey(date), image);
-            }
-            catch(e) {
-                console.log('Cache is full. ', e);
-            }
+        if(daysBetween(date, today) < CACHE_PERSIST_DAYS) {
+            // Cache and persist
+            trySet(localStorage, date, image);
+        }
+        else {
+            // Cache only
+            trySet(sessionStorage, date, image);
         }
 
+        // Routine cache cleanup
         free();
     };
 
     my.get = function(date) {
-        return localStorage.getItem(getKey(date));
+        var key = getKey(date);
+        return localStorage.getItem(key) || sessionStorage.getItem(key);
     };
 
     return my;
